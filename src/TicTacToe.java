4aae13b38adb2ae4df8e4daa37c5cff15ca238f5
Tristen Yim <yimt@rsd407.org>
2022-10-20 10:57:12 -0700

    TicTacToe get user input in theory but it doesn't quite work

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -16,40 +16,106 @@ Pseudocode:
 Maintenance Log:
     Started (19 Oct 2022 10:19)
     Finished testForWin and testForDraw but main doesn't do much of anything yet (19 Oct 2022 10:55)
+    Worked on the method to make a move. Currently the user input doesn't quite work because the book isn't good at explaining things, but it in theory works (20 Oct 2022 10:55)
 */
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class TicTacToe {
-    private static final char X = 'X', Y = 'Y';
-    public static void main(String[] args) {
+    private static final char X = 'X', O = 'O';
+    public static void main(String[] args)
+            throws InterruptedException {
         char[][] gameBoard = new char[3][3];
-
-        //...
-
+        int turn = 1;
+        while (true) {
+            if (turn % 2 == 1) {
+                makeMove(X, gameBoard);
+            } else {
+                makeMove(O, gameBoard);
+            }
+            if (testForDraw(gameBoard)) {
+                endCondition('\0');
+                return;
+            } else if (testForWin(X, gameBoard)) {
+                endCondition(X);
+                return;
+            } else if (testForWin(O, gameBoard)) {
+                endCondition(O);
+                return;
+            }
+            turn++;
+        }
     }
-    private static void testForDraw(char[][] gameBoard) {
+    private static void makeMove(char player, char[][] gameBoard)
+            throws InterruptedException {
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            printBoard(gameBoard);
+            fancyPrintLn("Make your move:");
+            if (input.hasNextInt()) {
+                int position = input.nextInt();
+                if (position < 10 && position > 0 && gameBoard[(position - 1) % 3][(position - 1) / 3] != '\0') {
+                    input.close();
+                    gameBoard[(position - 1) % 3][(position - 1) / 3] = player;
+                    return;
+                }
+            }
+            input.nextLine();
+        }
+    }
+    private static boolean testForDraw(char[][] gameBoard) {
         for (char[] i : gameBoard) {
             for (char j: i) {
-                if ('\0' == j) { return; }
+                if ('\0' == j) { return false; }
             }
         }
-        endCondition('\0');
+        return true;
     }
-    private static void testForWin(char playerToTest, char[][] gameBoard) {
+    private static boolean testForWin(char playerToTest, char[][] gameBoard) {
         // Using brute force to test the diagonals since it is only 2 tests
         if (playerToTest == gameBoard[0][0] && playerToTest == gameBoard[1][1] && playerToTest == gameBoard[2][2] ||
                 playerToTest == gameBoard[0][2] && playerToTest == gameBoard[1][1] && playerToTest == gameBoard[2][0]) {
-            endCondition(playerToTest);
+            return true;
         }
         // Using a loop to test the horizontals and verticals since it requires 6 tests
         for (int i = 0; i < 3; i++) {
-            if (playerToTest == gameBoard[i][0] && playerToTest == gameBoard[i][1] && playerToTest == gameBoard[i][2]) {
-                endCondition(playerToTest);
-            } else if (playerToTest == gameBoard[0][i] && playerToTest == gameBoard[1][i] && playerToTest == gameBoard[2][i]) {
-                endCondition(playerToTest);
+            if (playerToTest == gameBoard[i][0] && playerToTest == gameBoard[i][1] && playerToTest == gameBoard[i][2] ||
+                    playerToTest == gameBoard[0][i] && playerToTest == gameBoard[1][i] && playerToTest == gameBoard[2][i]) {
+                return true;
             }
         }
-        return;
+        return false;
     }
     private static void endCondition(char cause) {
+    }
+    private static void printBoard(char[][] gameBoard)
+            throws InterruptedException {
+        StringBuilder boardToPrint = new StringBuilder();
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard.length; j++) {
+                if (gameBoard[i][j] == '\0') {
+                    boardToPrint.append(i * 3 + j + 1);
+                } else {
+                    boardToPrint.append(gameBoard[i][j]);
+                }
+                if (j < 2) {
+                    boardToPrint.append(" | ");
+                }
+            }
+            if (i < 2) {
+                boardToPrint.append("\n---------\n");
+            }
+        }
+        fancyPrintLn(boardToPrint.toString());
+    }
+    private static void fancyPrintLn(String stringToPrint)
+            throws InterruptedException {
+        for (int i = 0; i < stringToPrint.length(); i++) {
+            System.out.print(stringToPrint.charAt(i));
+            Thread.sleep(20);
+        }
+        Thread.sleep(500);
+        System.out.println();
     }
 }
