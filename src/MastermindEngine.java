aa6cc93ef4b31938d37d6f409b3909b5ae49b41d
Tristen Yim <yimt@rsd407.org>
2022-11-04 09:56:38 -0700

    Working on the engine (4 Nov 2022 9:55)

--- src/MastermindEngine.java
+++ src/MastermindEngine.java
@@ -6,7 +6,26 @@ Purpose:
     To play mastermind in the most optimal way
 Pseudocode:
 Maintenance Log:
+    Started (4 Nov 2022 9:42)
+    Didn't get much done, though started working on making the program fill the arraylist with all possible codes (4 Nov 2022 9:56)
 */
 
+import java.util.ArrayList;
+
 public class MastermindEngine {
+    ArrayList<String> possibleCodes = new ArrayList<String>();
+    public void MastermindEngine(int colors) {
+        for (int i = 0; i < colors; i++) {
+            for (int j = 0; j < colors; j++) {
+                for (int k = 0; k < colors; k++) {
+                    for (int l = 0; j < colors; l++) {
+                        possibleCodes.add(i + j + k + l);
+                    }
+                }
+            }
+        }
+    }
+    public static void eliminatePossibleCodes(int[] pins) {
+
+    }
 }
--- src/MastermindGame.java
+++ src/MastermindGame.java
@@ -8,40 +8,61 @@ Maintenance Log:
     Started the game UI (31 Oct 2022 10:12)
     Started setting up the variables and methods but didn't get to make much actual code (31 Oct 2022 10:40)
     Now takes input but doesn't do error checking quite right yet (3 Nov 2022 10:56)
+    Finished (4 Nov 2022 9:56)
 */
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class MastermindGame {
     private static final Random R = new Random();
-    private static final int allowedAttempts = 13;
+    private static final int ALLOWED_ATTEMPTS = 13;
     public static void main (String[] args) {
         Scanner consoleInput = new Scanner(System.in);
         String secretCode = String.valueOf(R.nextInt(10)) + String.valueOf(R.nextInt(10)) +
                 String.valueOf(R.nextInt(10)) + String.valueOf(R.nextInt(10));
-        int attempt = 1;
-        while (true) {
+        int attempt = 0;
+        while (attempt < ALLOWED_ATTEMPTS) {
+            System.out.println("You have " + (ALLOWED_ATTEMPTS - attempt) + " attempts left!");
             if (guess(consoleInput, secretCode)) {
-
+                System.out.println("You win!");
+                return;
             }
+            attempt++;
         }
+        System.out.println("You lose! The code was " + secretCode);
     }
     private static boolean guess(Scanner consoleInput, String secretCode) {
         while (true) {
             System.out.print("Please input your guess: ");
             String guessedCode = consoleInput.next();
-            if (guessedCode.length() < 4 ) {
+            String convertedCode = toCode(guessedCode);
+            if (convertedCode.equals("")) {
                 continue;
             }
-            for (int i = 0; i < 3; i++) {
-                if (guessedCode.charAt(i) < 48 || guessedCode.charAt(i) > 57) {
-                    continue;
-                }
+            MastermindTester.StudentAlgorithm scoreAlgo = new MastermindTester.TristenYim2(10);
+            int[] score = scoreAlgo.scoreCodewords(secretCode, convertedCode);
+            System.out.println(Arrays.toString(score));
+            if (score[0] == 4) {
+                return true;
             }
-            System.out.println(guessedCode);
-            MastermindTester.StudentAlgorithm score = new MastermindTester.TristenYim2(10);
-            score.scoreCodewords(secretCode, guessedCode.substring(0, 4));
+            return false;
         }
+    }
+    private static String toCode(String stringToTest) {
+        if (stringToTest.length() < 4) {
+            return "";
+        }
+        StringBuilder digits = new StringBuilder();
+        for (int i = 0; i < stringToTest.length() && i < 4; i++) {
+            if (stringToTest.charAt(i) > 47 && stringToTest.charAt(i) < 58) {
+                digits.append(stringToTest.charAt(i));
+            }
+        }
+        if (digits.length() < 4) {
+            return "";
+        }
+        return digits.toString();
     }
 }
